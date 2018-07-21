0fbfad4b5c2200b3d6730ec1d75d0697cff8530f
donatok <donatokava@gmail.com>
2018-07-21 19:01:13 -0400

    added small explanation to file

--- first.scala
+++ first.scala
@@ -1,3 +1,5 @@
+// my first attempt at writing a scala program out side of REPL
+
 import scala.io._
 val display_message = StdIn.readLine("enter message ")
 println(s"The message entered was ${display_message}")
