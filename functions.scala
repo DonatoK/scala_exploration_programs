f8b974242531dc2e869a3972a87d014eeb0d7f63
donatok <donatokava@gmail.com>
2018-07-22 20:30:01 -0400

    firt try

---
+++ functions.scala
@@ -0,0 +1,20 @@
+// playing with functions in scala
+
+import scala.io._
+// function 1: signum function, returns 1 for a postive 0 for a negitive
+
+val display_message = StdIn.readLine("enter a number: ")
+
+def signum(x: UnInt): = val zero = "zero"
+			val positive = "positive"
+   			val negitive = "negitive"
+			if (x<0)	
+			negitive		
+			if(x === 0)
+			zero
+			if (x>=1)
+			positive
+			 
+signum(display_message)
+	
+println(s" The significant number is " )
