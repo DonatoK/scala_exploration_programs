357305110bd7cab992c1bf9148965038abb3094f
donatok <donatokava@gmail.com>
2018-07-24 22:52:49 -0400

    tried a while loop as a replacement for a for loop

--- functions.scala
+++ functions.scala
@@ -3,7 +3,11 @@
 import scala.io._
 // function 1: signum function, returns if a entered number is postive, 0, or negitive
 
-val display_message = StdIn.readLine("enter a number: ").toInt	
+//used a while loop to do for(i=10, i>=0; i--) to repeat signum function
+
+//writing a procedure which returns nothing and counts down form 10
+
+var repeat_limit = 10
 
 def signum(x: Int) = 
 			if (x<0)	
@@ -12,6 +16,18 @@ def signum(x: Int) =
 			"zero"
 			else if (x>=1)
 			"positive"
-			 
-val result = signum(display_message)
-println(s" The significant number is " + result)
+
+def countdown(n: Int)
+{
+
+
+}
+
+while(repeat_limit>=0)
+{
+
+  val display_message = StdIn.readLine(repeat_limit+" enter a number: ").toInt
+  val result = signum(display_message)
+  println(s" The significant number is " + result)
+  repeat_limit -= 1
+}
