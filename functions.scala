399c8ad1b72aa1904b5fe39d45b248c32e03ec52
donatok <donatokava@gmail.com>
2018-07-29 14:23:40 -0400

    attempt at making a loopless string product

--- functions.scala
+++ functions.scala
@@ -2,6 +2,7 @@
 
 import scala.io._
 import scala.Char._
+import scala.collection.immutable._
 // function 1: signum function, returns if a entered number is postive, 0, or negitive
 
 // used a while loop to do for(i=10, i>=0; i--) to repeat signum function
@@ -9,6 +10,11 @@ import scala.Char._
 // writing a procedure which returns nothing and counts down from a given value.i
 
 // stringAddder: function  to add the integer values of an input string. size of string
+
+// stringProduct: function to produce the product of all the valude in a string.
+
+// stringProductLoopless: same as stringProduct but with no loop. tried stringOps but the result is weird.
+
 
 var repeat_limit = 2
 
@@ -44,13 +50,22 @@ def stringAdder(inputString: String) =
 def stringProduct(inputString: String) =
 {
                         val charArry = inputString.toCharArray
-                        var sum      = 1
+                        var product      = 1
                         for(i <- 0 to inputString.length -1)
                         {               
-                          sum = sum * char2int(charArry(i))
+                          product = product * char2int(charArry(i))
                         } 
-                        sum
+                        product
 }
+
+def stringProductLoopless(inputString: String) =
+{
+                        val stringOpper = new StringOps(inputString)
+                        val product     = stringOpper.product
+   			product.toInt	 
+                        product
+}
+
 
 while(repeat_limit>=0)
 {
@@ -65,5 +80,6 @@ val hello_value = stringAdder("Hello")
 println(s" hello equals  " + hello_value)
 val hello_value2 = stringProduct("Hello")
 println(s" hello equals " + hello_value2)
-
+val hello_value3 = stringProductLoopless("Hello")
+println(s" hello equals " + hello_value3)
 
