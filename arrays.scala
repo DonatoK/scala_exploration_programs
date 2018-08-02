a5f88a42b0f13a3bdd25dcad39bf1cff8d01491f
donatok <donatokava@gmail.com>
2018-08-02 08:02:49 -0400

    ' figured out returning function arrarys

--- arrays.scala
+++ arrays.scala
@@ -1,9 +1,12 @@
 import scala.collection.mutable.ArrayBuffer
+import scala.util._
+
 // randomArray: varible array of size 0 to n filled with random numbers. wanted to return the array but it
-                  prints it terminal.
+//                 prints it terminal.
 
+// arraySwapAdjenct: swaps adjenct values in an array
 
-def randomArray(size: Int)
+def randomArray(size: Int) : ArrayBuffer[Int] =
 {
   var countUp    = 0
   var varArray    = ArrayBuffer[Int]()
@@ -15,13 +18,13 @@ def randomArray(size: Int)
     }
   // wanted to return this from the function but
   // printing worked.
-  for( i <- 0 until varArray.length)
-  println(s"$i: ${varArray(i)}")
-
+    for( i <- 0 until varArray.length)
+      println(s"$i: ${varArray(i)}")
+  varArray
 }
 
 val printArray = randomArray(3)
-//println(s" 3 random Numbers:  ")      ***planned but did not work***
+//println(s" 3 random Numbers:  ")         ***planned but did not work***
 //for( i <- 0 until printArray.length)
 //  println(s"$i: ${printArray(i)}")
 
