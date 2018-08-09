a27dd6bf2698b413a6f0b9e8a484c89cf2850a85
donatok <donatokava@gmail.com>
2018-08-08 22:56:02 -0400

    cleaned up command line entry. removed swap files

--- maps_and_truples.scala
+++ maps_and_truples.scala
@@ -1,3 +1,4 @@
+
 // Author: Donato Kava, Dated and created: 8/7/2019 
 // Scala map and trouple exploration file.
 
@@ -13,25 +14,23 @@ var run_UI = "run"
 //////////////////////
 //   " in printf   //
 ////////////////////
-printf( "enter \"1\" to use (Item, Cost), Discount function. Enter the number of items names, and their names comma seperated by their values. ex: \"name\" 1. Then the percent discounted. " )
+printf( "Enter \"1\" to use (Item, Cost), Discount function. \n")
+printf( " Enter the number of items names, and their names \n comma seperated by their values. ex: \"name\" 1. Then the percent discounted. \n " )
 
 while (run_UI != "quit")
 {
-  printf("How many items with values are there? ")
+  printf("  How many items with values are there? ")
   val how_Many_Items = StdIn.readLine().toInt
   /////////////////////
   //   Blank Map     //
   /////////////////////
-  //var maped_items = scala.collection.mutable.Map[String,Double]()               
-  //var maped_items = scala.collection.mutable.Map[String](Double)
   var maped_items = Map[String,Double]()
   for ( i <- 0 until how_Many_Items)   
   {  
     val user_input_key     = StdIn.readLine("Enter an item name as a string ")
     val user_input_value   = StdIn.readLine("Enter the items price ").toDouble
     
-    //maped_items(user_input_key) = user_input_value
-    maped_items = maped_items + ((user_input_key, user_input_value))//scala.collection.mutable.Map((user_input_key, user_input_value)) 
+    maped_items = maped_items + ((user_input_key, user_input_value)) 
   }
   
   val user_input_percent = StdIn.readLine("enter percent as integer ").toInt.toDouble
